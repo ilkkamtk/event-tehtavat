cce1ef2eeee6d0ff0f6ff386f17cf30a4dece45e
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2021-11-24 20:38:24 +0200

    Update teht4A.js

--- js/teht4A.js
+++ js/teht4A.js
@@ -1,11 +1,11 @@
 const pics = [
   {
-    thumb: 'http://www.fillmurray.com/100/100',
-    big: 'http://www.fillmurray.com/640/480',
+    thumb: 'https://www.fillmurray.com/100/100',
+    big: 'https://www.fillmurray.com/640/480',
   },
   {
-    thumb: 'http://lorempixel.com/100/100/sports/1/',
-    big: 'http://lorempixel.com//640/480/sports/1/',
+    thumb: 'https://lorempixel.com/100/100/sports/1/',
+    big: 'https://lorempixel.com//640/480/sports/1/',
   },
   {
     thumb: 'https://placeimg.com/100/100/tech',
